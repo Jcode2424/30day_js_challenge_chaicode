201bc2667120a49187faf79d7764b3f22d4eeeb7
2024coding <2024coding@gmail.com>
2024-07-24 17:46:41 +0900

    day 10

---
+++ day10.js
@@ -0,0 +1,10 @@
+// Day 10: Event Handling
+// Achievements:
+//  1. Add and handle basic events like click, double-click, mouseover, mouseout, keydown and keyup.
+//  2. Understand and handle form events.
+//  3. Implement event delegation to manage events on dynamically added elements.
+//  4. Make web pages interactive by responding to various user actions.
+
+//Activity 1: Understanding Promises
+//Task 1:
+//Task 2:
